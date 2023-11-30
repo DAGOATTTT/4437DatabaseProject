20b995667b51240c6f3c9748335fed81fce1f752
[DAGOATTTT] <104139331+DAGOATTTT@users.noreply.github.com>
2023-11-29 21:27:44 -0600

    endpoints for getmovie and createuser done

--- backend/controllers/MovieController.js
+++ backend/controllers/MovieController.js
@@ -1,28 +1,29 @@
 const express = require('express')  
 const router = express.Router()
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 
-const con = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: `${process.env.mysqlpasswd}`,
-    database: "moviesproject"
-})
+// const con = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password: `${process.env.mysqlpasswd}`,
+//     database: "moviesproject",
+//     // rowsAsArray: true 
+// })
 
-function testConnection(connection){
-    connection.connect(function(err){
-        if(err) throw err;
-        console.log("Connected!")
-    });
-}
+// function testConnection(connection){
+//     connection.connect(function(err){
+//         if(err) throw err;
+//         console.log("Connected!")
+//     });
+// }
 
 
 //endpoint: /movies
 //POST
 const createMovies = async(req, res) => {
-    const {title, description} = req.body
+    const {title, description, date, time, showings } = req.body
 
-    if(!title || !description) {
+    if(!title || !description || !date || !time || !showings) {
         res.status(400);
         throw new Error("Please add all fields")
     }
@@ -41,11 +42,31 @@ const createMovies = async(req, res) => {
 }
 
 
+
+
 //endpoint: /movies
 // GET
 const getmovies = async(req, res) => {
-    console.log(process.env.mysqlpasswd)
-    testConnection(con);
+    // create the connection
+    const connection = await mysql.createConnection({host:'localhost', user: 'root', password: `${process.env.mysqlpasswd}`, database: 'moviesproject'});
+    let obj = ""
+    let obj2 = ""
+
+    obj = await connection.execute('SELECT MovieID, title FROM `Movie`');
+    
+    obj2 = await connection.execute('SELECT Showing.MovieID, Movie.title, Start_Time, Datetime  FROM  Showing, Movie WHERE Showing.MovieID = Movie.MovieID ')
+    //getting the first index of each query object
+    movieobj = obj[0];
+    movieobj2 = obj2[0];
+    console.log(movieobj)
+    console.log(movieobj2)
+
+    let result = {
+        // ...movieobj,
+       ...movieobj2
+    }
+
+      res.status(200).json({movies: [result]})
 }
 
 
--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -1,11 +1,18 @@
+const { randomInt } = require('crypto');
 const express = require('express')  
 const router = express.Router()
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 
 
 //endpoint: /users
 // POST
 const createuser = async(req, res) => {
+    let UserID = randomInt(1,1000)
+    const connection = await mysql.createConnection({host:'localhost', user: 'root', password: `${process.env.mysqlpasswd}`, database: 'moviesproject'});
+    const {name, password, email, Phone_Number} = req.body;
+
+    userobj = await connection.execute(`INSERT INTO User(UserID, name, password, email, Phone_Number) VALUES(${UserID}, '${name}', ${password}, '${email}', ${Phone_Number})`);
+
     res.status(200).json({message:"success"})
 }
 
