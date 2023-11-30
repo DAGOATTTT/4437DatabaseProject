70e5f1e68d85fa7ea02ab847342227ee932f6e72
nhunguyen16015 <nhunguyen16015@gmail.com>
2023-11-29 21:14:12 -0600

    add api handling

--- frontend/movie-booking-app/src/pages/login.js
+++ frontend/movie-booking-app/src/pages/login.js
@@ -6,6 +6,40 @@ import { useRouter } from 'next/router'
 export default function LoginPage( {username} ) {
     const router = useRouter()
     const { msg } = router.query
+
+    const handleLogin = async (event) => {
+        event.preventDefault();
+    
+        // Get the form data
+        const formData = new FormData(event.target);
+        const username = formData.get('username');
+        const password = formData.get('password');
+    
+        try {
+          // Make the API call to check user's email and password
+          const response = await fetch('http://localhost:5000/users/login', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ username, password }),
+          });
+    
+          if (response.ok) {
+            // Successful login, you may want to handle the response accordingly
+            router.push('/'); // Redirect to the home page after successful login
+          } else {
+            // Handle unsuccessful login
+            const errorData = await response.json();
+            console.error('Login failed:', errorData);
+            router.push('/login?msg=Login failed');
+          }
+        } catch (error) {
+          console.error('Error during login:', error);
+          router.push('/login?msg=An error occurred during login');
+        }
+      };
+
     return (
         <Layout pageTitle="Login">
             <Link href="/">Home</Link><br/>
@@ -15,7 +49,7 @@ export default function LoginPage( {username} ) {
                 <></>
             }
             <h2>Log in</h2>
-            <form action='/api/login' method='POST'>
+            <form onSubmit={handleLogin}>
                 <input minLength="3" name="username" id="username" type="text" placeholder='username' required></input><br/>
                 <input minLength="5" name="password" id="password" type="password" placeholder='password' required></input><br/>
                 <input type="submit" value="Login"/>
--- frontend/movie-booking-app/src/pages/signup.js
+++ frontend/movie-booking-app/src/pages/signup.js
@@ -3,23 +3,67 @@ import { getCookie } from 'cookies-next';
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 
-export default function SignupPage( {username} ) {
+export default function SignupPage({ username }) {
     const router = useRouter()
     const { msg } = router.query
+
+    const handleSignup = async (event) => {
+        event.preventDefault();
+
+        const formData = new FormData(event.target);
+        const username = formData.get('username');
+        const password = formData.get('password');
+        const passwordagain = formData.get('passwordagain');
+
+        // Check if passwords match
+        if (password !== passwordagain) {
+            console.error('Passwords do not match');
+            return;
+        }
+
+        // Call the API
+        try {
+            const response = await fetch('http://localhost:5000/users/signup', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    email: username, // Assuming username is the email
+                    password: password,
+                }),
+            });
+
+            if (response.ok) {
+                // Successful signup
+                const data = await response.json();
+                console.log('Signup successful:', data);
+                router.push('/'); // Redirect to home page or wherever you want
+            } else {
+                // Handle signup error
+                const errorData = await response.json();
+                console.error('Signup error:', errorData.message);
+            }
+        } catch (error) {
+            console.error('Error during signup:', error);
+        }
+    };
+
+
     return (
         <Layout pageTitle="Signup">
-            <Link href="/">Home</Link><br/>
+            <Link href="/">Home</Link><br />
             {msg ?
                 <h3 className="red">{msg}</h3>
-            :
+                :
                 <></>
             }
             <h2>Sign up</h2>
-            <form action='/api/signup' method='POST'>
-                <input minLength="3" name="username" id="username" type="text" placeholder='username' required></input><br/>
-                <input minLength="5" name="password" id="password" type="password" placeholder='password' required></input><br/>
-                <input minLength="5" name="passwordagain" id="passwordagain" type="password" placeholder='password again' required></input><br/>
-                <input type="submit" value="Signup"/>
+            <form onSubmit={handleSignup}>
+                <input minLength="3" name="username" id="username" type="text" placeholder='username' required></input><br />
+                <input minLength="5" name="password" id="password" type="password" placeholder='password' required></input><br />
+                <input minLength="5" name="passwordagain" id="passwordagain" type="password" placeholder='password again' required></input><br />
+                <input type="submit" value="Signup" />
             </form>
         </Layout>
     );
@@ -29,7 +73,7 @@ export async function getServerSideProps(context) {
     const req = context.req
     const res = context.res
     var username = getCookie('username', { req, res });
-    if (username != undefined){
+    if (username != undefined) {
         return {
             redirect: {
                 permanent: false,
@@ -37,5 +81,5 @@ export async function getServerSideProps(context) {
             }
         }
     }
-    return { props: {username:false} };
+    return { props: { username: false } };
 };
